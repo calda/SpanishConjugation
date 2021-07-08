e49a244cb25b6b68d5ec980cddbbf5b22eefb12d
Cal Stephens <cal@calstephens.tech>
2021-07-08 09:30:50 -0700

    Initial Commit

---
+++ Shared/SpanishConjugationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpanishConjugationApp.swift
+//  Shared
+//
+//  Created by Cal Stephens on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SpanishConjugationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
